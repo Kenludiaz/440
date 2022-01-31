35635919d992989941e45ad0c006d909b9887623
Kenneth Lu Diaz <ludiazken@gmail.com>
2022-01-31 11:25:15 -0800

    Keeps track of engaged and unengaged

--- A1/marriage.py
+++ A1/marriage.py
@@ -25,20 +25,68 @@ def handleInput():
         exit(1)
 
 def match(unmatchedKnights, unmatchedLadies):
-    for knight in unmatchedKnights.copy():
-        # Stores the name of the first lady in the knight's list
-        lady = unmatchedKnights[knight][0]
-        if unmatchedLadies[lady][0] == knight:
-            removeFromLists(unmatchedKnights, unmatchedLadies, knight, lady)
-            print(unmatchedKnights)
-            print(unmatchedLadies)
+    matched = {}
+    engagedKnights = []
+    unenagedKnights = [*unmatchedKnights.keys()]
 
-# Removes knights and ladies from unmatched and the lists of all the others
-def removeFromLists(unmatchedKnights, unmatchedLadies, luckyKnight, luckyLady):
+    engagedLadies = []
+
+    for x in range(1,20):
+        for knight in unenagedKnights:
+            # Stores the name of the first lady in the knight's list
+            lady = unmatchedKnights[knight][0]
+
+            # YES, there are no knights of higher preference in the lady's list
+            if unmatchedLadies[lady][0] == knight:
+                removeFromLists(matched, unmatchedKnights, unmatchedLadies, knight, lady)
+                unenagedKnights.remove(knight)
+
+            # MAYBE, not yet proposed to
+            elif (lady not in engagedLadies):
+                engagedKnights.append(knight)
+                engagedLadies.append(lady)
+                unenagedKnights.remove(knight)
+                # Remove knights of lower preference
+                # removeLowerKnights(unmatchedLadies, lady, knight)
+
+            # MAYBE, was proposed to by a knight of higher preference
+            elif (knight in unmatchedLadies[lady]):
+                # Undo the previous engagement
+                index = engagedLadies.index(lady)
+                engagedLadies.pop(index)
+                leftoverKnight = engagedKnights.pop(index)
+                unenagedKnights.append(leftoverKnight)
+
+                # Re-propose
+                # Lady is removed and then re-added to have the pairings share an index 
+                engagedKnights.append(knight)
+                engagedLadies.append(lady)
+
+
+            # # NO, the lady already has a knight of higher preference
+            # Remove knights of lower preference
+
+
+    # The people that were engaged will now finally tie the knot and will have the same index
+
+    print(matched)
+
+# Removes knights and ladies from unmatched and the preference lists of all the others
+def removeFromLists(matched, unmatchedKnights, unmatchedLadies, luckyKnight, luckyLady):
     unmatchedKnights.pop(luckyKnight)
     unmatchedLadies.pop(luckyLady)
     for i in unmatchedKnights:
-        oldList = unmatchedKnights[i]
-        newList = oldList.remove(luckyLady)
+        unmatchedKnights[i].remove(luckyLady)
+    for j in unmatchedLadies:
+        unmatchedLadies[j].remove(luckyKnight)
+
+    matched[luckyKnight] = luckyLady
+
+# Removes knights with a lower preference than the one given
+def removeLowerKnights(unmatchedLadies, lady, knight):
+    index = unmatchedLadies[lady].index(knight)
+
+    for i in range(len(unmatchedLadies), index, -1):
+        print("Removed element", unmatchedLadies[lady].pop())
 
 main()
