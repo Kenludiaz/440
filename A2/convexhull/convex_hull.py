bb89c4e101c1e4411a3ed55c62998ce024deee59
Kenneth Lu Diaz <ludiazken@gmail.com>
2022-02-17 15:44:47 -0500

    Update convex_hull.py
    Fixes base case when there is one point

--- A2/convexhull/convex_hull.py
+++ A2/convexhull/convex_hull.py
@@ -132,7 +132,8 @@ def base_case_hull(points: List[Point]) -> List[Point]:
                 if points[j] not in hull:
                     hull.append(points[j])
     # Sort Clockwise
-    clockwise_sort(hull)
+    if len(hull) > 1:
+        clockwise_sort(hull)
     return hull
 
 def finger_merge(left_hull: List[Point], right_hull: List[Point]) -> List[Point]:
