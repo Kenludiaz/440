25e4d7ed7446882d922c61e5100b2173ec9937c6
Kenneth Lu Diaz <ludiazken@gmail.com>
2022-03-07 10:01:39 -0800

    Adds invariants

--- rubik/solver.py
+++ rubik/solver.py
@@ -32,23 +32,27 @@ def shortest_path(
     start_current_states.append( start )
     end_current_states.append(    end  )
 
+    # Initialization: start_configs + rev(inv(end_configs)) is equal to the correct set
+    # of permutations towards the end state up to i for i, 1->7. Trivially true, because
+    # the first step will always just be the beginning and ending states.
     for i in range(7):
         start_new_states = []
         end_new_states   = []
-    #     Record past moves & current node for start
+    # Maintnence: For every loop there will be at least 6^i - i permutations in start_configs and 
+    # end_configs.
+
         for current in start_current_states:
-            past_moves = start_configs.get(current) # []
+            past_moves = start_configs.get(current) 
 
             for move in rubik.quarter_twists:
                 new_cube = rubik.perm_apply(move, current)
                 current_moves = past_moves + [move]
-
                 # Saving the shortest path
                 if new_cube not in start_configs:
-                    start_configs.update({new_cube: current_moves}) # a: [F]
+                    start_configs.update({new_cube: current_moves}) 
                     start_new_states.append(new_cube) 
 
-                # Checking if we found and answer
+                # Checking if we found the answer
                 if new_cube in end_configs:
                     start_list = start_configs.get(new_cube)
                     inverted_list = list(map(rubik.perm_inverse, end_configs.get(new_cube)))
@@ -58,7 +62,6 @@ def shortest_path(
 
         start_current_states = start_new_states
 
-    #     Record past moves & current nodes for end
         for current in end_current_states:
             past_moves = end_configs.get(current)
 
@@ -71,15 +74,15 @@ def shortest_path(
                     end_configs.update({new_cube: current_moves})
                     end_new_states.append(new_cube)
 
-                # Checking if we found and answer
+                # Checking if we found the answer
                 if new_cube in start_configs:
                     start_list = start_configs.get(new_cube)
                     inverted_list = list(map(rubik.perm_inverse, end_configs.get(new_cube)))
                     inverted_list.reverse()
                     return start_list + inverted_list  
-
             past_moves = []
+        # Termination: We either find matching permutations on both side, with up to i == 7 layers,
+        # or we find that the configuration is physically impossible.
         end_current_states = end_new_states
-        
 
     return None
